e56afe09d0634e0806c4d9dc1feefa804e235914
ishwarkumar09 <159995811+ishwarkumar09@users.noreply.github.com>
2025-02-09 18:55:40 +0000

    convertion and operations

---
+++ basics/convertionAndOperations.js
@@ -0,0 +1,36 @@
+
+// <==========================Number=========================>
+let age = 14
+let score = "12"
+
+
+
+// console.log(typeof score)
+let valueInScore = Number(score)
+// console.log(valueInScore)
+
+// console.log(typeof NaN) result => type of Nan is number 
+// console.log(Number(null)) result => convertion of null in number is 0 and data type will come number;
+
+// console.log(Number(true)) // result => value will be 1 and data type number
+// console.log(Number(false)) // result => value will be 0 and data type number
+
+// console.log(Number(undefined)) // result => value will be NaN and data type number
+
+
+// <======================================boolean==========================================>
+
+    const isLoggedin = null
+
+    const boolIsLoggedIn = Boolean(isLoggedin)
+
+    console.log(boolIsLoggedIn)
+    
+    // <=================result==================>
+    // ==> for value 1 convert true and 0 convert false data type boolean
+    // ==> when number is greater than 1 than comes true 
+    // ==> when empty string than come false and when when is any string available than true
+    // ==> when value is null than convertion is false
+
+
+
