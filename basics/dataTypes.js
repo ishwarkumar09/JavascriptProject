774c618f2712dd451bbe6a176d8f494d3a437c5e
ishwarkumar09 <159995811+ishwarkumar09@users.noreply.github.com>
2025-02-09 17:11:58 +0000

    add file of data type

---
+++ basics/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" ;// treat all JS code as newer version
+
+// alert("hello") this will run in only browser because in browser engine is written in document but in done there is not available
+
+
+// number => 2 to 53
+// string
+// bigInt => when number is more that number 
+// null => standalon data type
+// undefine => not define value 
+// symbol => unique
+
+// object 
+const age = 10
+
+
+// console.log(typeof null) result => data type is object will show when we console data type of the null?
+// console.log(typeof undefined); result => data type of undefine is undefine
+
+
