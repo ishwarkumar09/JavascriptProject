e288da19c125155441748010114dd95982afe3e0
ishwarkumar09 <159995811+ishwarkumar09@users.noreply.github.com>
2025-02-10 04:52:13 +0000

    add operations

--- basics/convertionAndOperations.js
+++ basics/convertionAndOperations.js
@@ -24,13 +24,54 @@ let valueInScore = Number(score)
 
     const boolIsLoggedIn = Boolean(isLoggedin)
 
-    console.log(boolIsLoggedIn)
+    // console.log(boolIsLoggedIn)
     
     // <=================result==================>
     // ==> for value 1 convert true and 0 convert false data type boolean
     // ==> when number is greater than 1 than comes true 
     // ==> when empty string than come false and when when is any string available than true
     // ==> when value is null than convertion is false
+
+
+// ********************** OPERATIONS ***************************
+
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+// let str1 = 'hello'
+// let str2 = ' ishwar'
+
+// let str3 = str1 + str2
+// console.log(str3); // str3 result is hello ishwar
+
+// console.log("1" + 2); // result will come 12
+// console.log(1 + "2"); // result will come 12
+
+// console.log("1" +2 +2);// result will come 122
+// console.log(1 + 2 + "2") ; //result will come 32
+
+// console.log(+true); // result will be 1
+
+
+
+let gamescore = 100
+
+// console.log(++gamescore)  //prefix
+// console.log(gamescore++) // postfix
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%<==>%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 
 
 
