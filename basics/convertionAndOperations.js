dbca92e1e8f77286c3d8d5760ae85ea2c325b816
ishwarkumar09 <159995811+ishwarkumar09@users.noreply.github.com>
2025-02-14 10:21:43 +0000

    add file converstion code

--- basics/convertionAndOperations.js
+++ basics/convertionAndOperations.js
@@ -70,7 +70,19 @@ let gamescore = 100
 
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%<==>%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
 
+
+// the reason it that an equality check == and comparisons >< >= <= work differently
+// comparisons convert null to a number , treating it as 0 .
+// that's why null >= 0 is true and null > 0 is false
+
+
+// ===
+
+console.log("2" === 2)
 
 
 
